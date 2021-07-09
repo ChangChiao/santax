8f5ade09ff2fd75cb9a578a55964c108240b5606
joe <joe.chang1014@gmail.com>
2021-07-09 21:03:18 +0800

    新增手錶資料json

---
+++ src/components/Crumbs.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <span>首頁</span>
+        <span>></span>
+        <span>{{ series }}</span>
+        <span>></span>
+        <span>{{ model }}</span>
+    </div>
+</template>
+
+<script>
+import watchData from "/watch.json";
+export default {
+    watch: {
+        "$route.query.id": {
+            immediate: true,
+            handler: function (val) {
+                const [ser, mod] = val.split("-");
+                this.series = this.seriesList[ser];
+                this.model = mod;
+            },
+        },
+    },
+    data() {
+        return {
+            seriesList: {
+                e: "工程師系列",
+                g: "GMT系列",
+                d: "潛水系列",
+                t: "鐵道系列",
+            },
+            series: "",
+            model: "",
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
