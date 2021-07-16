96abed14a0cf7be015375184791150c7e48a60de
joe <joe.chang1014@gmail.com>
2021-07-16 20:27:06 +0800

    問題一覽

--- src/views/Question.vue
+++ src/views/Question.vue
@@ -1,9 +1,149 @@
 <template>
-    <div></div>
+    <div class="question">
+        <div
+            class="slider"
+            style="background-image: url(/images/banner/homepage_find.jpg)"
+        >
+            <div class="slider__content">
+                <h1 class="slider__content__title">常見問題</h1>
+                <p class="slider__content__text">
+                    SANTAX針對不同的使用習慣，提供您各種日常問題的解說。
+                </p>
+            </div>
+        </div>
+        <div class="title">
+            <h2>問題一覽</h2>
+        </div>
+        <div class="question__main">
+            <div class="question__main__pic">
+                <img src="/images/watch/e/e-9-0.png" alt="" />
+            </div>
+            <ul class="question__main__content">
+                <li
+                    class="question__main__content__item"
+                    v-for="(vo, i) in list"
+                    :key="'q' + i"
+                    @click="setActive(i)"
+                >
+                    <p>{{ vo.question }}</p>
+                    <p class="question" v-if="nowIndex === i">
+                        {{ vo.answer }}
+                    </p>
+                </li>
+            </ul>
+            <div class="question__main__pic">
+                <img src="/images/watch/t/t-4-0.png" alt="" />
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            list: [
+                {
+                    question: "Q1.服務和維修",
+                    answer: "請前往任何一家SANTAX腕錶專賣店或授權零售店。 所有SANTAX腕錶專賣店及授權零售商地址均可見于我們的網站 當製錶技師徹底檢查過您的腕錶後，您將收到一份報價表，其中會列明預計歸還日期。請注意，此過程需費時十二天。",
+                },
+                {
+                    question: "Q2.我的SANTAX腕錶多久需保養一次？",
+                    answer: "在日常佩戴中，腕錶承受許多不同的負荷與壓力，例如震動、撞擊、灰塵、水分、紫外線輻射、汗水以及溫度波動。腕錶機芯需要每天24小時精準走時並持續精確運作多年的時間，因此機芯也承受著巨大的機械負荷。為了解決某些活動零件的磨損以及機油和潤滑劑自然老化的問題，我們建議佩戴者大約每兩年將腕錶送交進行一次期中服務，大約每五年進行一次全面服務。",
+                },
+                {
+                    question: "Q3.如何分辨SANTAX腕錶是否為真品？",
+                    answer: "SANTAX腕錶會為每枚出廠的腕錶進行註冊，以作永久保存。自1885年以來，機芯、所用材質及錶殼的詳細資料均記錄在冊，而後期錶款的記錄內容亦包括參考編號，因此對照序號即可分辨真偽",
+                },
+                {
+                    question: "Q4.SANTAX腕錶的防水性能如何？",
+                    answer: "SANTAX腕錶除了潛水錶款，皆具備生活防水功能，您在進行任何非常規活動（ 團體運動、跳水運動等等）以前， 應將腕錶脫下並置於安全地方。請注意並非所有類型的錶帶或錶鏈（ 取決於選用材質）皆適用於水中或水下。此外， 除了潛水員腕錶以外， 其他錶款的活動零件（ 如錶冠、按鈕等等）皆不宜在水下操作。在配戴腕錶進入水中或水下以前， 請先確定腕錶的所有活動零件都已回到原位。",
+                },
+                {
+                    question: "Q5.磁場會影響我的SANTAX腕錶嗎？",
+                    answer: "原則上，您應避免讓腕錶暴露在磁場中，無論時間長短。任何機械機芯都包含可被磁化的零件，是否受磁取決於磁場的強度。德國工業標準DIN 8309規定，如果機械腕錶暴露於強度為4800 A/m（安培/米）（相當於60.3奧斯特）的磁場中，之後未出現功能明顯受損的跡象，則該機械腕錶屬於防磁腕錶。",
+                },
+                {
+                    question: "Q6.可以配戴皮革錶帶游泳嗎？",
+                    answer: "皮革是一種天然材質，具有滲透性。因此，您應避免讓優質皮革錶帶與水分、油類、溶劑及清潔品或化妝品發生接觸。這樣可以防止皮革材質變色或過早老化。",
+                },
+                {
+                    question: "Q7.為什麼SANTAX沒有天文台錶？",
+                    answer: "天文台錶是指機芯精確度得到瑞士官方天文台（COSC）檢測中心認可的腕錶。SANTAX腕錶採用錶廠自己的標準對腕錶進行調校及測試。除此以外，佩戴者的使用習慣和生活方式也會影響腕錶的精確度，其作用並非不重要。在肢體活動較多的佩戴者和生活較安靜的人士手腕上，自動機芯的運轉表現也不盡相同。任何偏離所標示容差範圍的情況並不完全是功能的問題，更主要是調校和佩戴習慣的問題。",
+                },
+                {
+                    question: "Q8.可以戴著SANTAX腕錶運動嗎？",
+                    answer: "原則上，您應避免劇烈的振動和撞擊（例如打高爾夫球或網球、從事越野自行車運動等）。機械腕錶是一種精密儀器，在製作過程中需要達到數百分之一甚至數千分之一毫米的容差標準。反覆猛烈撞擊會對機芯的精確度造成不良影響，甚至會損壞機芯。",
+                },
+                {
+                    question: "Q9.SANTAX腕錶機芯為何？",
+                    answer: "腕錶內所使用的「寶石」並非天然珍貴寶石， 而是由工業用紅寶石製成，作用是降低機械日常運作時的磨損，並且主要用於軸承、槓桿、棘爪機構及用作擒縱叉。儘管製錶行業在過去曾使用天然紅寶石，然而現在則基本使用特性相同、同質性更高的合成紅寶石。",
+                },
+            ],
+            nowIndex: -1,
+        };
+    },
+    methods: {
+        setActive(index) {
+            this.nowIndex = index;
+        },
+    },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.question {
+    &__main {
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 100px;
+        &__pic {
+            width: 15%;
+            overflow: hidden;
+            position: relative;
+            height: 100%;
+            img {
+                position: relative;
+                left: -150%;
+            }
+        }
+        &__content {
+            width: 70%;
+            &__item {
+                @for $i from 0 through 9 {
+                    &:nth-child(#{$i}) {
+                        p {
+                            background: lighten($base, $i * 3%);
+                        }
+                    }
+                }
+                p {
+                    background: $base;
+                    cursor: pointer;
+                    width: 90%;
+                    height: 100px;
+                    text-align: center;
+                    line-height: 100px;
+                    margin: auto;
+                    color: #fff;
+                    text-shadow: 0px 1px #aaa;
+                    transition: 0.5s;
+                    border-radius: 15px;
+                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+                    &.question {
+                        height: auto;
+                        text-align: left;
+                        line-height: 1.5rem;
+                        box-sizing: border-box;
+                        padding: 30px;
+                        font-size: 0.8rem;
+                    }
+                    &:hover {
+                        transform: scale(1.05);
+                    }
+                }
+            }
+        }
+    }
+}
+</style>
